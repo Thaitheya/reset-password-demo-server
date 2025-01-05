01bfc3699fc6ffcdb75234e0723360abc1866b91
Thaitheyasudan P K <91272336+Thaitheya@users.noreply.github.com>
2025-01-05 15:27:52 +0530

    Add files via upload
    Updated server

--- server.js
+++ server.js
@@ -14,5 +14,5 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use("/api/auth", authRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = 5000
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
